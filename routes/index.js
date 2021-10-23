2b13205fce6adb1f3138d40ef05ae94c4c5911b8
Ty Eggen <tycho.eggen@gmail.com>
2021-10-23 02:04:39 -0700

    routes/index.js: - request -> axios - async/await - proper try/catch

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var rp = require('request-promise');
 var vagueTime = require('vague-time');
+var axios = require('axios')
 
 /*
  * This is now using flat text.
@@ -11,25 +11,26 @@ var vagueTime = require('vague-time');
  * 
  */
 
-router.get('/', function(req, res, next) {
-  rp('https://api.vanhack.ca/s/vhs/data/laser.json')
-  .then(function (payload) {
-    var laser_status = JSON.parse(payload);
+router.get('/', async function(req, res, next) {
+  try {
+    const result = await axios('https://api.vanhack.ca/s/vhs/data/laser.json')
+
+    var laser_status = result.data
 
     var current_time = Math.floor(new Date().getTime() / 1000);
     var time_changed = laser_status.last_updated;
     var vague_changed = vagueTime.get({from:current_time, to:time_changed, units:'s'});
+  
     if( laser_status.value == "off" ) {
       res.render('index', { title: 'Is The Laser Busy', message: 'The laser is not busy! Last usage was ' + vague_changed + '.' });
     } else {
       res.render('index', { title: 'Is The Laser Busy', message: 'The laser is in use and has been in use since ' + vague_changed + '.' });
     }
-  })
-  .catch(function (err) {
+  } catch(err) {
     // Crawling failed...
     res.render('index', { title: 'Is The Laser Busy', message: 'There was an error!' });
     console.dir( err );
-  });
-});
+  }
+})
 
 module.exports = router;
